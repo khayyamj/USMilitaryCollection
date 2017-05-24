1b29bc4e0bffdd850ea3751397b9724bae6043d6
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-05-24 15:49:48 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var isProd = process.env.NODE_ENV === "production"; // true or false
+var cssDev = ['style-loader','css-loader','sass-loader'];
+var cssProd = ExtractTextPlugin.extract({
+      fallback: "style-loader",
+      use: ["css-loader", "sass-loader"],
+      publicPath: "/dist"
+    });
+var cssConfig = isProd ? cssProd : cssDev;
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: cssConfig // changes based on production or development
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|ico)$/i,
+        use: 'file-loader?images/name=[name].[ext]&outputPath=images/'
+      },
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    // port: 9000,
+    hot: true,
+    stats: "errors-only",
+    open: true
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "app.css",
+      // disable: false // doesn't work with hot module replacement
+      disable: !isProd
+    }),
+    new HtmlWebpackPlugin({
+      title: 'React Seed Project w/Linting',
+      // minify: {
+      //   collapseWhitespace: true
+      // },
+      hash: true,
+      template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+}
